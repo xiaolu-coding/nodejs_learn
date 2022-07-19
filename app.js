d4c4732643b274d4cfca347c2e1ec6d027467a63
xiaolu-coding <454819375@qq.com>
2022-07-19 15:46:03 +0800

    chore: 一些响应写入

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  console.log(req.method);
-  console.log(req.headers);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>')
+  res.write('</html>')
+  res.end();
 })
 
 server.listen(3000);
